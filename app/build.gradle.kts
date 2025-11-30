8e82c0805df6e9ba735d61610301710a50f12f5c
VanTruong2509 <truongnv2561@ut.edu.vn>
2025-11-30 21:58:45 +0700

    update
    Removed unnecessary dependencies and comments, and updated some existing dependencies.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,8 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     id("org.jetbrains.kotlin.plugin.compose")
-    id("kotlin-kapt") // ✅ cần cho Room
-    kotlin("kapt")
+    kotlin("kapt") 
 }
 
 android {
@@ -16,7 +15,6 @@ android {
         targetSdk = 36
         versionCode = 1
         versionName = "1.0"
-
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
@@ -37,7 +35,6 @@ android {
         jvmTarget = "17"
     }
 
-    // ✅ Thêm đây để fix warning META-INF
     packaging {
         resources {
             excludes += listOf(
@@ -49,16 +46,13 @@ android {
     }
 }
 
-
 dependencies {
-    // Core
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
 
-    // Coil (load image)
     implementation("io.coil-kt:coil-compose:2.6.0")
 
     // Jetpack Compose BOM
@@ -67,42 +61,19 @@ dependencies {
     implementation("androidx.compose.ui:ui-tooling-preview")
     debugImplementation("androidx.compose.ui:ui-tooling")
     implementation("androidx.compose.material3:material3")
-    implementation("androidx.compose.material:material-icons-extended:1.7.0")
-    implementation("androidx.compose.ui:ui:1.7.2")
-    implementation("androidx.compose.material3:material3:1.3.0")
+    implementation("androidx.compose.material:material-icons-extended")
 
-    // Navigation Compose
     implementation("androidx.navigation:navigation-compose:2.8.3")
-
-    // ViewModel + Compose
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.6")
-
-    // Activity Compose
     implementation("androidx.activity:activity-compose:1.9.3")
-
-    // DataStore (Preferences)
     implementation("androidx.datastore:datastore-preferences:1.1.1")
 
-    // Room (SQLite)
     val room_version = "2.6.1"
-
     implementation("androidx.room:room-runtime:$room_version")
     kapt("androidx.room:room-compiler:$room_version")
     implementation("androidx.room:room-ktx:$room_version")
 
-    // Testing
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
-
-    implementation("androidx.compose.ui:ui:1.6.0")
-    implementation("androidx.compose.material3:material3:1.2.0")
-    implementation("androidx.compose.ui:ui-tooling-preview:1.6.0")
-    implementation("androidx.compose.foundation:foundation:1.6.0")
-
-    implementation("androidx.compose.runtime:runtime-livedata:1.6.8")
-
-    implementation("com.sun.mail:android-mail:1.6.7")
-    implementation("com.sun.mail:android-activation:1.6.7")
-
 }
