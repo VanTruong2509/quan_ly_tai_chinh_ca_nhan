2d3a14fd671960bf1c3938a425a0ef3e3d9270b4
VanTruong2509 <truongnv2561@ut.edu.vn>
2025-12-02 12:11:43 +0700

    update

--- app/src/main/java/com/example/moneyfy/data/model/Spending.kt
+++ app/src/main/java/com/example/moneyfy/data/model/Spending.kt
@@ -1,6 +1,11 @@
 package com.example.moneyfy.data.model
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "spendings")
 data class Spending(
+    @PrimaryKey(autoGenerate = true) val id: Int = 0,
     val amount: Float,
     val category: String = "Khác",
     val note: String = "",
