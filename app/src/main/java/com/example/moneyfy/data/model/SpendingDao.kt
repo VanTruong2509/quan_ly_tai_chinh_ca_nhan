c64836e15ca5a362ae46950ec78efb355465cc20
VanTruong2509 <truongnv2561@ut.edu.vn>
2025-12-05 00:38:41 +0700

    Update SpendingDao.kt

--- app/src/main/java/com/example/moneyfy/data/model/SpendingDao.kt
+++ app/src/main/java/com/example/moneyfy/data/model/SpendingDao.kt
@@ -1,18 +1,22 @@
 package com.example.moneyfy.data.model
 
-import androidx.room.Dao
-import androidx.room.Insert
-import androidx.room.Query
+import androidx.room.*
 
 @Dao
 interface SpendingDao {
-
+    
     @Insert
     suspend fun insertSpending(spending: Spending)
-
+    
     @Query("SELECT * FROM spendings ORDER BY timestamp DESC")
     suspend fun getAllSpendings(): List<Spending>
-
+    
     @Query("SELECT * FROM spendings ORDER BY timestamp DESC LIMIT 7")
     suspend fun getLatest7(): List<Spending>
+    
+    @Delete
+    suspend fun deleteSpending(spending: Spending)
+    
+    @Update
+    suspend fun updateSpending(spending: Spending)
 }
